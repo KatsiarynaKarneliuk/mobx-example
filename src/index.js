b2a7876c49c081bb02aa4f2c20cbe5a01e98a7c5
ekaterinkamx6 <v281107@gmail.com>
2021-10-16 15:20:05 +0300

    fixed the import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 /* import Words from './context'; */
 import { Provider } from 'mobx-react';
-import WordsStore from './stores';
+import WordsStore from './stores/wordsStore.jsx';
 import App from './App';
 
 const stores = {
